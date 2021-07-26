a9e5ea039f1907ef9fa0c88266dc8ec0e2b4e969
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2021-07-26 16:42:24 +0530

    first java program

---
+++ Week1(Git,Java)/java_demos/first_java_app_demo/src/mypack/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Heyyy");
+		System.out.print("I will not create a line break");
+		System.out.println(" Which means I will be in same line");
+		System.out.println("But not me");
+	}
+
+}
