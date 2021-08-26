b2de07a9a7b462a236a9d4a8b394a385c1fc1781
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2021-08-26 16:00:00 +0530

    context config demo

---
+++ Week4(HTML,CSS,JS,Servlet,REST)/servlet_demos/servlet_context_config_demo/src/main/java/com/app/Servlet1.java
@@ -0,0 +1,48 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<h2>Accessing ServletConfig params of Servlet1 in Servlet1</h2> ");
+		ServletConfig config=getServletConfig();
+		out.print("<h3>Query1 : "+config.getInitParameter("query1")+"</h3>");
+		out.print("<h3>Query2 : "+config.getInitParameter("query2")+"</h3>");
+		out.print("<h3>Query3 : "+config.getInitParameter("query3")+"</h3>");
+		
+		
+		out.print("<h2>Accessing ServletContext params in Servlet1</h2> ");
+		ServletContext context=getServletContext();
+		out.print("<h3>URL : "+context.getInitParameter("url")+"</h3>");
+		out.print("<h3>DRIVER : "+context.getInitParameter("driver")+"</h3>");
+	}
+
+}
