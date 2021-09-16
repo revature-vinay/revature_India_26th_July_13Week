e5a76b089d8e819780d5d4584d35c7a2e8bfea8a
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2021-09-16 15:34:36 +0530

    swagger

--- Week7(Spring,SpringBoot,Spring-Angular)/spring_boot_demos/spring_boot_jpa_lombok_h2_swagger_demo/src/main/java/com/app/SpringBootDemo2Application.java
+++ Week7(Spring,SpringBoot,Spring-Angular)/spring_boot_demos/spring_boot_jpa_lombok_h2_swagger_demo/src/main/java/com/app/SpringBootDemo2Application.java
@@ -3,7 +3,10 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+
 public class SpringBootDemo2Application {
 
 	public static void main(String[] args) {
